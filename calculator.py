32f99a7ef9524ce0f62358471b5d3b5175f8aa16
PukhalskaMaryna <pukhalskiy@ukr.net>
2025-02-24 21:49:13 +0200

    initial commit

---
+++ calculator.py
@@ -0,0 +1,19 @@
+def calculator(num1, num2, operation):
+    """
+    Реалізує простий калькулятор для двох чисел.
+
+    :param num1: Перше число.
+    :param num2: Друге число.
+    :param operation: Операція (додавання, віднімання, множення, ділення).
+    :return: Результат операції.
+    """
+    result = 0
+
+    return result
+
+# Перевірка
+assert calculator(5, 3, 'add') == 8
+'add'
+"subtract"
+"multiply"
+"divide"
