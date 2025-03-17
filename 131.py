cc6e2bbb94473f40686ee6751c4312184df9d651
PukhalskaMaryna <pukhalskiy@ukr.net>
2025-03-17 22:27:30 +0200

    initial commit

--- 131.py
+++ 131.py
@@ -5,6 +5,7 @@ class Human:
         self.first_name = first_name
         self.last_name = last_name
 
+
     def __str__(self):
         return f"Ім'я: {self.first_name} {self.last_name}, стать: {self.gender}, вік: {self.age} р."
 
