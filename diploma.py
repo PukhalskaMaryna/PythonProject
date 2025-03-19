5f21849de18d299049dc211d2b940af93347fa4b
PukhalskaMaryna <pukhalskiy@ukr.net>
2025-03-19 16:25:02 +0200

    initial commit

--- diploma.py
+++ diploma.py
@@ -2,14 +2,17 @@
 import sqlite3
 import csv
 import os
+from datetime import datetime
+import re
 
 class DB:
     def __init__(self, db_file_name='db.db'):
-        """
-        Клас для роботи з базою даних
 
-        :param db_file_name: str, назва файла бд, за замовчуванням db.db
-        :param conn: підключення до б
+        """
+        Клас для роботи з базою даних.
+
+        :param db_file_name: Str, path-like object, назва файлу бд, за замовчуванням db.db
+        :param conn: незадаваємий параметр, який в собі містить інфу, чи є конект, по замовчуванню конекта нема
         """
         self.db_file_name = db_file_name
         self.conn = None
@@ -20,92 +23,100 @@ class DB:
     def delete_db(self):
         """
         Видалення файлу бд
-        Перевіряється, чи існує файл, і якщо так, він видаляється.
+        https://www.geeksforgeeks.org/python-os-path-exists-method/
         """
         if os.path.exists(self.db_file_name):
             os.remove(self.db_file_name)
-            print(f"База даних {self.db_file_name} успішно видалена")
-        else:
-            print(f"База даних {self.db_file_name} не знайдена")
 
     def open_connection(self):
         """
         Підключення до бд
         https://docs.python.org/3/library/sqlite3.html#sqlite3.Connection
+        https://docs.python.org/3/library/sqlite3.html#sqlite3.Error
+        Важливо, що навіть якщо цей метод викликати на нестворену бд, тобто фактично файл з бд не існує,
+        то файл успішно створиться і конект буде з новоствореним файлом
         """
-        self.conn = sqlite3.connect(self.db_file_name)
+        try:
+            self.conn = sqlite3.connect(self.db_file_name)
+            return self.conn
+        except sqlite3.Error as sql_error:
+            print(f"Не вдалося підключитися до бд: {sql_error}")
+            return None
 
     def close_connection(self):
-        """Закриття з'єднання з бд"""
+        """
+        Закриття з'єднання з бд
+        https://docs.python.org/3/library/sqlite3.html#sqlite3.Connection.close
+        """
         if self.conn:
             self.conn.close()
-            self.conn = None
-            print("З'єднання з бд успішно закрите")
-        else:
-            print("З'єднання вже було закрите раніше")
+
+    def execute_query(self, query, params=()):
+        """
+        Метод запиту до бази даних
+        https://docs.python.org/3/library/sqlite3.html#cursor-objects
+
+        :param query: str, SQL-запит
+        :param params: tuple, параметри для запиту
+        :return: list, turtle, результат виконання запиту список кортежів
+        """
+        if not self.conn:
+            print("Не встановлено підключення")
+            return None
+
+        try:
+            my_cursor = self.conn.cursor()  # курсор
+            my_cursor.execute(query, params)  # виклик за запитом
+            self.conn.commit()  # commit
+            result = my_cursor.fetchall()  # результат запиту у вигляді списку кортежів, кортеж - рядок із таблиці
+            my_cursor.close()  # закриваємо курсор
+            return result
+        except sqlite3.Error as sql_error:
+            print(f"Помилка execute_query: {sql_error}")
+            return None
 
     def create_table(self, table, fields):
         """
         Створення таблиці з зазначеними полями.
 
         :param table: Str, назва таблиці
-        :param fields: dict, словник з полями та їх типами
+        :param fields: str, рядок з полями та їх типами
         приклад:
-        fields = {
-                    'title': 'TEXT NOT NULL',
-                    'year': 'INTEGER NOT NULL',
-                    'director': 'TEXT',
-                }
+        fields = 'title TEXT NOT NULL, year INTEGER NOT NULL, director TEXT'
         """
-        field_definitions = []
-        for field, field_type in fields.items():
-            field_definitions.append(f"{field} {field_type}")
 
-        # Додаємо поле id як PRIMARY KEY
-        field_definitions.insert(0, "id INTEGER PRIMARY KEY AUTOINCREMENT")
-        query = f"CREATE TABLE IF NOT EXISTS {table} ({', '.join(field_definitions)})"
-        self.execute_query(query)
+        fields = "id INTEGER PRIMARY KEY AUTOINCREMENT, " + fields # id як PRIMARY KEY
+        self.execute_query(f"CREATE TABLE IF NOT EXISTS {table} ({fields})")
 
     def drop_table(self, table):
         """
-        Створення таблиці з зазначеними полями.
+        Видалені таблиці
 
         :param table: Str, назва таблиці
         """
-        query = f"DROP TABLE IF EXISTS {table}"
-        self.execute_query(query)
+        self.execute_query(f"DROP TABLE IF EXISTS {table}")
 
     def insert_into_table(self, table, row):
         """
         Метод для вставки одного рядка
 
         :param table: Str, назва таблиці бд, в яку додаємо дані
-        :param row: tuple, кортеж значень для одного рядка (без id)
+        :param row: tuple, значення для одного рядка по кожному полю, крім id
+            приклад, ("Пухальська", "Марина", "Ж", "21.10.1983")
         """
-        if self.conn is None:
+        if not self.conn:
             print("Не встановлено підключення")
             return None
 
         try:
-            my_cursor = self.conn.cursor()  # Створення курсора
-            my_cursor.execute(f"SELECT MAX(id) FROM {table}")
-            result = my_cursor.fetchone()
-            next_id = result[0] + 1 if result[0] is not None else 1
+            result = self.execute_query(f"SELECT MAX(id) FROM {table}")
+            next_id = 1 if not result or result[0][0] is None else result[0][0] + 1
 
-            # Додаємо поле id до значень для вставки (row)
-            row_with_id = (next_id,) + row
-
-            my_cursor.execute(f"INSERT INTO {table} VALUES({', '.join(['?'] * len(row_with_id))})", row_with_id)
-            self.conn.commit()  # Збереження змін
-            my_cursor.close()  # Закриваємо курсор після завершення
-
+            # передаємо значення як параметри в SQL-запит
+            query = f"INSERT INTO {table} (id, last_name, first_name, middle_name, gender, birth_date) VALUES (?, ?, ?, ?, ?, ?)"
+            self.execute_query(query, (next_id, *row))
         except sqlite3.Error as sql_error:
-            print(f"Помилка виконання запиту: {sql_error}")
-            # приклади помилок:
-            # sqlite3.ProgrammingError - невірна к-ть полів
-            # sqlite3.IntegrityError - якщо вставити значення в унікальне поле (у нас це id)
-            # sqlite3.DatabaseError - проблема доступу до бд
-            # sqlite3.DataError - спроба вставити невірний тип даних
+            print(f"Помилка insert_into_table: {sql_error}")
 
     def delete_from_table(self, table, conditions):
         """
@@ -113,27 +124,23 @@ class DB:
 
         :param table: Str, назва таблиці бд, з якої видаляємо дані
         :param conditions: dict, словник з умовами видалення,
-                напр. {'id': 1} або {'last_name': 'Пухальська', 'first_name': 'Марина'}
+                приклад {'id': 1} або {'last_name': 'Пухальська', 'first_name': 'Марина'}
         """
-        if self.conn is None:
+        if not self.conn:
             print("Не встановлено підключення")
             return None
 
         try:
-            my_cursor = self.conn.cursor()  # Створення курсора
             condition_strings = []
             params = []
             for column, value in conditions.items():
-                condition_strings.append(f"{column} = ?")
-                params.append(value)
-            condition_clause = " AND ".join(condition_strings)
-            query = f"DELETE FROM {table} WHERE {condition_clause}"
-            my_cursor.execute(query, params)
-            self.conn.commit()
-            my_cursor.close()
+                condition_strings.append(f"{column} = ?")  # ['last_name = ?', 'first_name = ?']
+                params.append(value)  # ['Пухальська', 'Марина']
+            condition_clause = " AND ".join(condition_strings)  # 'last_name = ? AND first_name = ?'
 
+            self.execute_query(f"DELETE FROM {table} WHERE {condition_clause}", tuple(params))
         except sqlite3.Error as sql_error:
-            print(f"Помилка виконання запиту: {sql_error}")
+            print(f"Помилка delete_from_table: {sql_error}")
 
     def truncate_table(self, table_name):
         """
@@ -141,43 +148,14 @@ class DB:
 
         :param table_name: str, назва таблиці, яку потрібно очистити
         """
-        if self.conn is None:
+        if not self.conn:
             print("Не встановлено підключення")
             return None
 
         try:
-            my_cursor = self.conn.cursor()  # Створення курсора
-            query = f"DELETE FROM {table_name}"  # SQL-запит для очищення таблиці
-            my_cursor.execute(query)  # Виконання запиту
-            self.conn.commit()  # Збереження змін
-            my_cursor.close()  # Закриття курсора
-            print(f"Таблиця {table_name} успішно очищена.")
+            self.execute_query(f"DELETE FROM {table_name}")
         except sqlite3.Error as sql_error:
-            print(f"Помилка виконання запиту: {sql_error}")
-
-    def execute_query(self, query, params=()):
-        """
-        Метод запиту до бази даних
-        https://docs.python.org/3/library/sqlite3.html#sqlite3.Cursor.execute
-
-        :param query: str, SQL-запит
-        :param params: tuple, параметри для запиту
-        :return: результат виконання запиту
-        """
-        if self.conn is None:
-            print("Не встановлено підключення")
-            return None
-
-        try:
-            cursor = self.conn.cursor()  # Створення курсора
-            cursor.execute(query, params)  # Виконання запиту
-            self.conn.commit()  # Збереження змін
-            result = cursor.fetchall()  # Отримання результатів запиту
-            cursor.close()  # Закриваємо курсор після завершення
-            return result # Список кортежів-рядків бд
-        except sqlite3.Error as sql_error:
-            print(f"Помилка виконання запиту: {sql_error}")
-            return None
+            print(f"Помилка truncate_table: {sql_error}")
 
     def count_rows(self, table_name):
         """
@@ -186,18 +164,16 @@ class DB:
         :param table_name: Str, назва таблиці в базі даних
         :return: кількість рядків в таблиці
         """
-        if self.conn is None:
+        if not self.conn:
             print("Не встановлено підключення")
 
         try:
-            my_cursor = self.conn.cursor()  # Створення курсора
-            query = f"SELECT COUNT(*) FROM {table_name}"  # SQL-запит для підрахунку рядків
-            my_cursor.execute(query)  # Виконання запиту
-            row_count = my_cursor.fetchone()[0]  # Отримуємо результат (кількість рядків)
-            my_cursor.close()  # Закриваємо курсор після завершення
-            return row_count
+            result = self.execute_query(f"SELECT COUNT(*) FROM {table_name}")
+            if result:
+                return result[0][0]
+            return 0
         except sqlite3.Error as sql_error:
-            print(f"Помилка виконання запиту: {sql_error}")
+            print(f"Помилка count_rows: {sql_error}")
 
 #################################################################
 db1 = DB('db.db')
@@ -205,7 +181,7 @@ db1 = DB('db.db')
 # print(db1)
 db1.open_connection()
 db1.drop_table('clients')
-db1.create_table('clients',{'last_name':'TEXT','first_name':'TEXT','middle_name':'TEXT','gender':'TEXT','birth_date':'DATE'})
+db1.create_table('clients',"last_name TEXT,first_name TEXT, middle_name TEXT, gender TEXT, birth_date DATE")
 
 # my_table = 'clients'
 # cnt_rows = db1.count_rows(my_table)
@@ -234,31 +210,71 @@ class Client:
     def __str__(self):
         return f"{self.last_name} {self.first_name} {self.middle_name} - {self.gender} - {self.birth_date}"
 
-    def add_client(self, db = DB(), table = str(), import_from_csv=True):
+    def add_one_client(self, db=DB(), table=str()):
         """
-        Додавання клієнта
+        Додавання одного клієнта
 
         :param db: DB, підключення до бази даних
         :param table: str, назва таблиці
-        :param import_from_csv: bool, якщо True, додає клієнтів з csv
         :return: None
         """
-        if import_from_csv:
-            with open('clients.csv', newline='', encoding='utf-8') as csvfile:
-                for row in csv.DictReader(csvfile):
-                    client_row = (
-                        row["last_name"],
-                        row["first_name"],
-                        row["middle_name"],
-                        row["gender"],
-                        row["birth_date"]
-                    )
-                    db.insert_into_table(table, client_row)
-        else:
-            row = (self.last_name, self.first_name, self.middle_name, self.gender, self.birth_date)
-            db.insert_into_table(table, row)
 
-    def search_clients(self, db = DB(), table = str(), export_to_csv=True):
+        def is_valid_date(date_str):
+            date_pattern = r'^\d{2}\.\d{2}\.\d{4}$'
+            return not bool(re.match(date_pattern, date_str))
+
+        if not is_valid_date(self.birth_date):
+            correct_birth_date = self.birth_date
+        else:
+            try:
+                correct_birth_date = datetime.strptime(self.birth_date, "%Y-%m-%d").strftime("%d.%m.%Y")
+            except ValueError:
+                print(f"Помилка: невірний формат дати для клієнта {self.last_name}. Пропускаємо.")
+                return
+
+        row = (self.last_name, self.first_name, self.middle_name, self.gender, correct_birth_date)
+        db.insert_into_table(table, row)
+
+    @staticmethod
+    def add_client_from_csv(db=DB(), table=str(), my_file_name='import_clients.csv'):
+        """
+        Додавання клієнтів із csv
+
+        :param db: DB, підключення до бази даних
+        :param table: str, назва таблиці
+        :param my_file_name: назва csv для імпорту, за замовчуванням 'import_clients.csv'
+        :return: None
+        """
+
+        def is_valid_date(date_str):
+            date_pattern = r'^\d{2}\.\d{2}\.\d{4}$'
+            return not bool(re.match(date_pattern, date_str))
+
+        if not os.path.exists(my_file_name):
+            print(f"Файл '{my_file_name}' відсутній")
+            return
+
+        with open(my_file_name, newline='', encoding='utf-8-sig') as csv_file:
+            for row in csv.DictReader(csv_file):
+                # а чи всі поля у файлі?
+                if 'last_name' not in row or 'first_name' not in row or 'middle_name' not in row or 'gender' not in row or 'birth_date' not in row:
+                    print("Невірний формат файлу")
+                    continue
+
+                if is_valid_date(row['birth_date']):
+                    birth_date = row['birth_date']  # Залишаємо без змін, якщо дата вже у вірному форматі
+                else:
+                    try:
+                        birth_date = datetime.strptime(row['birth_date'], "%d.%m.%Y").strftime("%Y-%m-%d")
+                    except ValueError:
+                        print(
+                            f"Помилка: невірний формат дати для клієнта {row['id']}. Пропускаємо.")
+                        continue
+
+                client = Client(row["last_name"], row["first_name"], row["middle_name"], row["gender"], birth_date)
+                client.add_one_client(db, table)
+
+    def find_clients(self, db=DB(), table=str(), export_to_csv=True):
         """
         Пошук клієнтів
 
@@ -283,41 +299,45 @@ class Client:
             conditions.append("gender = ?")
             params.append(self.gender)
         if self.birth_date:
+            birth_date = datetime.strptime(self.birth_date, "%d.%m.%Y").strftime("%Y-%m-%d")
             conditions.append("birth_date = ?")
-            params.append(self.birth_date)
+            params.append(birth_date)
 
         if not conditions:
-            print("Не задано жодного параметра для пошуку.")
+            print("Вкажіть хоч 1 параметр для пошуку")
             return []
 
         condition_clause = " AND ".join(conditions)
-        result = db.execute_query(f"SELECT * FROM {table} WHERE {condition_clause}", tuple(params))
+        query = f"SELECT * FROM {table} WHERE {condition_clause}"
+        result = db.execute_query(query, tuple(params))
 
         clients = {}
         for row in result:
             client = Client(
-                last_name   =row[1],
-                first_name  =row[2],
-                middle_name =row[3],
-                gender      =row[4],
-                birth_date  =row[5]
+                last_name=row[1],
+                first_name=row[2],
+                middle_name=row[3],
+                gender=row[4],
+                birth_date=row[5]
             )
             client_id = row[0]
             clients.update({client_id: client})
 
-        if export_to_csv: # експорт у csv
+        # csv
+        if export_to_csv:
             file_name = 'found_clients.csv'
             i = 1
-            while os.path.exists(file_name): #якщо файл з такою назвою є, то додає цифру
+            while os.path.exists(file_name):  # якщо файл з такою назвою вже існує, додаємо цифру
                 file_name = f'found_clients_{i}.csv'
                 i += 1
 
             with open(file_name, mode='w', newline='', encoding='utf-8-sig') as file:
                 writer = csv.writer(file)
-                writer.writerow(["last_name", "first_name", "middle_name", "gender", "birth_date"])
-                for client_id,client in clients.items():
-                    writer.writerow([client_id, client.last_name, client.first_name
-                                        , client.middle_name, client.gender,client.birth_date])
+                writer.writerow(["id", "last_name", "first_name", "middle_name", "gender", "birth_date"])
+                for client_id, client in clients.items():
+                    writer.writerow([client_id, client.last_name, client.first_name,
+                                     client.middle_name, client.gender, client.birth_date])
+
         return clients
 
     def delete_client(self, db = DB(), table = str()):
@@ -328,8 +348,7 @@ class Client:
         :param db: DB, підключення до бази даних
         :param table: str, назва таблиці
         """
-        # Пошук клієнтів за атрибутами цього клієнта
-        clients_to_delete = self.search_clients(db, table, export_to_csv=False)
+        clients_to_delete = self.find_clients(db, table, export_to_csv=False)
 
         if clients_to_delete:
             for client_id, client in clients_to_delete.items():
@@ -349,18 +368,24 @@ clients_table = 'clients'
 
 db1.truncate_table(clients_table)
 
-cl1.add_client(db1,clients_table,False)
-cl2.add_client(db1,clients_table,False)
-cl3.add_client(db1,clients_table,False)
+cl1.add_one_client(db1,clients_table)
+cl2.add_one_client(db1,clients_table)
+cl3.add_one_client(db1,clients_table)
 
-cl1.search_clients(db1,clients_table)
-Client(gender='Ж').search_clients(db1,clients_table)
-Client(gender='Ч').search_clients(db1,clients_table)
-Client(birth_date='21.10.1983',gender='Ж').search_clients(db1,clients_table)
+cl1.find_clients(db1,clients_table)
+Client(gender='Ж').find_clients(db1,clients_table)
+Client(gender='Ч').find_clients(db1,clients_table)
+Client(birth_date='21.10.1983',gender='Ж').find_clients(db1,clients_table)
 
 
 print(db1.count_rows(clients_table))
 Client(gender='Ж').delete_client(db1,clients_table)
+print(db1.count_rows(clients_table))
+
+Client.add_client_from_csv(db1, 'clients', 'found_clients_1.csv')
+Client.add_client_from_csv(db1, 'clients', 'found_clients_2.csv')
+Client.add_client_from_csv(db1, 'clients', 'found_clients_1.csv')
+
 print(db1.count_rows(clients_table))
 
 db1.close_connection()
@@ -475,3 +500,6 @@ db1.close_connection()
 #
 #
 # create_form()
+
+
+
